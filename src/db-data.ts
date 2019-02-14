57055809269d296ed1e3957b7958c4c447f651dc
Dariusz Sibik <sibikdariusz@gmail.com>
2019-02-14 22:00:59 +0100

    *ngIf, else, elvis operator

--- src/db-data.ts
+++ src/db-data.ts
@@ -2,6 +2,8 @@
 
 export const COURSES: any = [
 
+    undefined,
+
     {
         id: 1,
         description: "Angular Core Deep Dive",
@@ -13,7 +15,7 @@ export const COURSES: any = [
     {
         id: 2,
         description: "RxJs In Practice Course",
-        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png',
+        iconUrl: '',
         longDescription: "Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples",
         category: 'BEGINNER',
         lessonsCount: 10
