e57f306c1d5ad0e21dc30d577a4ba8f13ae50ed4
Dariusz Sibik <sibikdariusz@gmail.com>
2019-02-16 02:05:50 +0100

    add pipe number, percent, title, date, price, keyvalue, slice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent {
 
 courses = COURSES;
 
+title = COURSES[1].description;
+
+price = 9.99564654;
+
+rate = 0.67;
+
+course = COURSES[1];
+
+startDate = new Date(2000, 0, 1);
+
   onCourseSelected(course:Course) {
 
     console.log("App component - click event bubbled....", course);
